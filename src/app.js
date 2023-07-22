fb4956427c3b7c4db2739136047558631cc600d1
Vakil.Verma <vakil@nimapinfotech.com>
2023-07-22 17:45:49 +0530

    router added

--- src/app.js
+++ src/app.js
@@ -3,14 +3,39 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
+import About from "./components/About";
+import { createBrowserRouter, RouterProvider,Outlet } from "react-router-dom";
+import Error from "./components/Error";
+import Contact from "./components/Contact";
 const AppLayout = () => {
   return (
     <React.Fragment>
       <Header />
-      <Body />
+      <Outlet/>
       <Footer />
     </React.Fragment>
   );
 };
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact/>
+      },
+    ],
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,33 @@
 import React, { useEffect, useState } from "react";
-import Logo from '../assets/img/logo.png';
+import Logo from "../assets/img/logo.png";
+import { Link } from "react-router-dom";
 const Title = () => {
   return (
     <a href="/">
-      <img
-        // src="https://lh5.googleusercontent.com/p/AF1QipOhHeCaQ6Xb6RVf3R_ZBTbDK4FIug_203rKsHLT"
-        src={Logo}
-        alt="logo"
-        className="logo"
-      />
+      <img src={Logo} alt="logo" className="logo" />
     </a>
   );
 };
-// const loginUser = () => {
-//   return true;
-// };
 const HeaderComponent = () => {
-  const [isLoggedIn,setIsLoggedIn]=useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
   return (
     <div className="header">
       <Title />
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact us</li>
-          <li>Cart</li>
+          <Link to={'/'}><li>Home</li></Link>
+          <Link to="/about">
+            <li>About Us</li>
+          </Link>
+          <Link to={'/contact'}><li>Contact us</li></Link>
+          <Link to={'/cart'}><li>Cart</li></Link>
         </ul>
       </div>
-      {/* <h1>
-      {((a = 10), console.log("====================================", a))}  //expression we can use it on jsx but not statement if condition is statement so that's why we can't use 
-    </h1> */}
-      {isLoggedIn ? <button onClick={()=>setIsLoggedIn(false)}>Logout</button> : <button onClick={()=>setIsLoggedIn(true)}>Login</button>}
+      {isLoggedIn ? (
+        <button onClick={() => setIsLoggedIn(false)}>Logout</button>
+      ) : (
+        <button onClick={() => setIsLoggedIn(true)}>Login</button>
+      )}
     </div>
   );
 };
